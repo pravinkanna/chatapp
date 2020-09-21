d8d9e96231cd495696e4af3dff9120fdabce8244
Pravin kanna P <mp.pravinkanna@gmail.com>
2020-09-21 21:02:01 +0530

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>whatsapp</h1>
+
+      {/* Sidebar */}
+
+      {/* Chat */}
+
+    </div>
+  );
+}
+
+export default App;
